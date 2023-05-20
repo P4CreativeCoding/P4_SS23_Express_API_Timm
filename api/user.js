bab2e7a57ddf53444976d49c5a9a87007f3611e7
Lisa Timm <lisa.suchsdorf@gmail.com>
2023-05-20 17:22:58 +0200

    test

--- api/user.js
+++ api/user.js
@@ -1,5 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3003;
+
 export default function handler(req, res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
   res.json({ name: "John Doe" });
 }
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
